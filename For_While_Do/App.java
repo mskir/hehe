7dbd624c8092075049ecee8c63b249484cc023ca
Journey Gamas <111472903+mskir@users.noreply.github.com>
2022-10-03 10:31:01 +0800

    Create App.java

---
+++ For_While_Do/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    for (int i = 3; i != 0; i--) {
+            System.out.println("Journey");
+        }
+    int surname = 3;
+
+        while (surname != 0) {
+            System.out.println("Gamas");
+            surname--;
+        }
+    int nickname = 3;
+    
+    do {
+        System.out.println("Journ");
+        nickname--;
+        } while (nickname != 0);
+}
+}
+
